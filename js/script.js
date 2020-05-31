eff4aa5ef718f40d7cfd9b67656e6104a0bf9fc9
kareem osman <58007559+kareem-osman@users.noreply.github.com>
2020-05-31 13:46:01 +0200

    vt

--- js/script.js
+++ js/script.js
@@ -11,4 +11,14 @@ $(function(){
         {scrollTop:0},800
     )
   })
+  $("textarea.code").click(function(e){
+    console.log(e.target)
+    e.target.select()
+    document.execCommand("copy")
+  })
+  $("textarea.color").click(function(e){
+    console.log(e.target)
+    e.target.select()
+    document.execCommand("copy")
+  })
   })
